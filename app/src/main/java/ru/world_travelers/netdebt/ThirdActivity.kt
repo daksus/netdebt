a66b6a9faa17420b0aaf17cd765a982be46b3966
Denis <denis_aksyonov@mail.ru>
2018-04-05 16:55:00 +0300

    добавил окна

---
+++ app/src/main/java/ru/world_travelers/netdebt/ThirdActivity.kt
@@ -0,0 +1,12 @@
+package ru.world_travelers.netdebt
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ThirdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
