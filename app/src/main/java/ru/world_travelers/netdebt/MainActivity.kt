f543cd89706af6014d80ba31a666ac9bf62c0430
Denis <denis_aksyonov@mail.ru>
2018-04-06 16:29:48 +0300

    добавил окна

--- app/src/main/java/ru/world_travelers/netdebt/MainActivity.kt
+++ app/src/main/java/ru/world_travelers/netdebt/MainActivity.kt
@@ -1,12 +1,24 @@
 package ru.world_travelers.netdebt
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Party (view: View)
+    {
+        val ttt = view.id.toString()
+        val myToast = Toast.makeText(this, ttt, Toast.LENGTH_SHORT)
+
+        myToast.show()
+
+
     }
 }
