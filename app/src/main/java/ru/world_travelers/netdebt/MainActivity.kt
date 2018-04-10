01dfb5f3f06a0ee24affb112ff17a3b8d15a2e91
Denis <denis_aksyonov@mail.ru>
2018-04-10 16:57:16 +0300

    добавил окна

--- app/src/main/java/ru/world_travelers/netdebt/MainActivity.kt
+++ app/src/main/java/ru/world_travelers/netdebt/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.world_travelers.netdebt
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +13,9 @@ class MainActivity : AppCompatActivity() {
 
     fun Party (view: View)
     {
-        val ttt = view.id.toString()
-        val myToast = Toast.makeText(this, ttt, Toast.LENGTH_SHORT)
+       //bThree.visibility = "none"
+       // visibility="invisible"
 
-        myToast.show()
 
 
     }
