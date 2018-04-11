b43ff21088356435b93da036a9bb39603d85d061
Denis <denis_aksyonov@mail.ru>
2018-04-11 13:57:40 +0300

    добавил окна

--- app/src/main/java/ru/world_travelers/netdebt/MainActivity.kt
+++ app/src/main/java/ru/world_travelers/netdebt/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.world_travelers.netdebt
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun Party (view: View)
-    {
-       //bThree.visibility = "none"
-       // visibility="invisible"
+    fun Party(view: View) {
+        //var tmp = findViewById<>(R.id.bTwo)
 
+        // bThree.visibility = View.INVISIBLE
+
+
+        when (view.id) {
+            R.id.bTwo -> {
+                val randomIn = Intent(this, SecondActivity::class.java)
+                randomIn.putExtra("user", "huihefuihew" )
+                startActivity(randomIn)
+
+            }
+            R.id.bThree -> {
+                textView.setText("3")
+            }
+            R.id.bFour -> {
+                textView.setText("4")
+            }
+        }
 
 
     }
