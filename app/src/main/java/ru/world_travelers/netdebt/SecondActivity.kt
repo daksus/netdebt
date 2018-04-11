e4cc7b8188b38f3cfcfa445f5ef407cf987567fb
Denis <denis_aksyonov@mail.ru>
2018-04-11 18:01:42 +0300

    добавил окна

--- app/src/main/java/ru/world_travelers/netdebt/SecondActivity.kt
+++ app/src/main/java/ru/world_travelers/netdebt/SecondActivity.kt
@@ -3,6 +3,7 @@ package ru.world_travelers.netdebt
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -23,7 +24,14 @@ class SecondActivity : AppCompatActivity() {
 
     fun fСonfirm (view: View) {
 
+        val name1 = edName1.text
+        val name2 = edName2.text
+        val name3 = edName3.text
+        val name4 = edName4.text
 
-        
+        val myToast = Toast.makeText(this, name1.toString(), Toast.LENGTH_SHORT)
+        myToast.show()
+
+
     }
 }
